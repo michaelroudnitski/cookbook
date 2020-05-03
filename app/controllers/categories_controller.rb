8ba1ee9201afee22ebe9db586c72b616446aaed9
Michael Roudnitski <michaelroudnitski@gmail.com>
2020-05-02 23:37:03 -0400

    fixed oopsie

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @recipes = Recipe.where(category_id: params[:id])
+    @recipes = @category.recipes
   end
 
   def destroy
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :comments
+  has_many :recipes, dependent: :delete_all
   ICONS = ["bread", "cookies", "cupcake", "flour", "pancakes", "pie", "rice", "steak"].map { |s| "#{s}.png" }
   validates_inclusion_of :icon, :in => ICONS
   validates :name, :icon, presence: true
